d88e6e174a9396e6b64555eea4efbff8c3baccfb
biiglol <46738034+biiglol@users.noreply.github.com>
2020-07-28 18:22:56 +0900

    chapter6-4 done

---
+++ src/App6-2.js
@@ -0,0 +1,58 @@
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
+    getMovies = async () => {
+        // 자바스크립트에게 getMovies() 함수는 시간이 필요하고
+        // const movies = await axios.get(
+        //     // axios.get()의 실행을 기다려 달라고 말해주는거임
+        //     "https://yts-proxy.now.sh/list_movies/json"
+        // );
+
+        const {
+            data: {
+                data: { movies },
+            },
+        } = await axios.get(
+            "https://yts-proxy.now.sh/list_movies/json?sort_by=rating"
+        );
+
+        this.setState({ movies: movies, isLoading: false }); // this.setState({ movies })  키와 대입할 변수의 이름이 같으니 movies로 축약 가능
+    };
+    componentDidMount() {
+        // setTimeout(() => {
+        //     this.setState({ isLoading: false });
+        // }, 6000);
+        // axios.get("https://yts-proxy.now.sh/list_movies/json");
+        this.getMovies();
+    }
+    render() {
+        const { isLoading, movies } = this.state;
+        return (
+            <div>
+                {isLoading
+                    ? "Loading..."
+                    : movies.map((movie) => {
+                          console.log(movie);
+                          return (
+                              <Movie
+                                  key={movie.id}
+                                  id={movie.id}
+                                  year={movie.year}
+                                  title={movie.title}
+                                  summary={movie.summary}
+                                  poster={movie.medium_cover_image}
+                              />
+                          );
+                      })}
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./App4";
-import App from "./App5-4";
+import App from "./App6-2";
 
 ReactDOM.render(<App />, document.getElementById("root"));
